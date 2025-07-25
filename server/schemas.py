27a29bca227fddb236564d9b3f2d336b413ade89
Shelby Christensen <shelbchrist@yahoo.com>
2025-07-25 16:32:55 -0500

    Add Marshmallow schemas for Exercise, Workout, and WorkoutExercise

---
+++ server/schemas.py
@@ -0,0 +1,23 @@
+from marshmallow import Schema, fields, validates, ValidationError
+from models import Workout, Exercise, WorkoutExercise
+
+class ExerciseSchema(Schema):
+    id = fields.Int(dump_only=True)
+    name = fields.Str(required=True)
+    category = fields.Str(required=True)
+    equipment_needed = fields.Bool(required=True)
+
+class WorkoutExerciseSchema(Schema):
+    id = fields.Int(dump_only=True)
+    workout_id = fields.Int(required=True)
+    exercise_id = fields.Int(required=True)
+    reps = fields.Int()
+    sets = fields.Int()
+    duration_seconds = fields.Int()
+
+class WorkoutSchema(Schema):
+    id = fields.Int(dump_only=True)
+    date = fields.Date(required=True)
+    duration_minutes = fields.Int(required=True)
+    notes = fields.Str()
+    workout_exercises = fields.Nested(WorkoutExerciseSchema, many=True)
